05b705d7458bc5fb34f86b7e4d5baa16978bf7e0
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-29 10:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+var audio;
+
+function playSound(filename) {
+	stopSound();
+	audio = new Audio("sounds/" + filename);
+	audio.play();
+}
+
+function stopSound() {
+	if (audio) {
+		audio.pause();
+		audio.currentTime = 0;
+	}
+}
